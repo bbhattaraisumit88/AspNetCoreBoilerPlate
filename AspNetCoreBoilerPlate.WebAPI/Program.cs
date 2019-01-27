ecc00e4a36880f3b42eb1d3acd1b6f2fac4c34cf
bsumitiw <bsumitiw@gmail.com>
2019-01-27 17:41:25 +0545

    Called innitial userr class at program.cs

--- AspNetCoreBoilerPlate.WebAPI/Program.cs
+++ AspNetCoreBoilerPlate.WebAPI/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AspNetCoreBoilerPlate.EFCore;
+using AspNetCoreBoilerPlate.WebAPI.Helpers;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace AspNetCoreBoilerPlate.WebAPI
 {
@@ -14,7 +11,22 @@ namespace AspNetCoreBoilerPlate.WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var hostBuilder = CreateWebHostBuilder(args);
+            var host = hostBuilder.Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    DbInitializer.InitializeAsync(context, services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
