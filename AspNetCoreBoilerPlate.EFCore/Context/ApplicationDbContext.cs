4a3de586d37bff440c60afdfafe2323f9d7df3c3
bsumitiw <bsumitiw@gmail.com>
2019-01-27 11:30:54 +0545

    Registered userrole and userclaims class in dbcontext

--- AspNetCoreBoilerPlate.EFCore/Context/ApplicationDbContext.cs
+++ AspNetCoreBoilerPlate.EFCore/Context/ApplicationDbContext.cs
@@ -1,15 +1,22 @@
 ﻿using AspNetCoreBoilerPlate.Domain.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace AspNetCoreBoilerPlate.EFCore
 {
-    public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid, AppUserClaim, AppUserRole, IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
